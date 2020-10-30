818c01b98c2dececbff5e8251dd519eff064b37e
derentho <thomas.derencourt@gmail.com>
2020-10-30 16:47:24 +0100

    refactor: ajout d'un dossier models

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import * as config from "./config.json";
-import { CourseArray } from "./course_array";
+import { CourseArray } from "./models/course_array";
 
 const app = express();
 
