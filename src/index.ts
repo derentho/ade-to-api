6f23073fbfa094c83f4d67e935fbcf2946cac2f5
derentho <thomas.derencourt@gmail.com>
2020-11-02 18:29:52 +0100

    refactor: passage sur Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
-import * as config from "./config.json";
 import CourseArray from "./models/course_array";
+
+const PORT = process.env.PORT || 8080;
+const SOURCE = "http://adecampus.univ-rouen.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources={{ rid }}&projectId=0&calType=ical&nbWeeks=52";
 
 const app = express();
 
 app.get("/:rid", async (req, res) => {
   const rid = Number.parseInt(req.params.rid);
-  const source = config.source.replace("{{ rid }}", `${rid}`);
+  const source = SOURCE.replace("{{ rid }}", `${rid}`);
   const data = await CourseArray.fromURL(source);
   res.json(data.orderByDate().json);
 })
 
-app.listen(config.port, () => {
-  console.log(`Listening at http://localhost:${config.port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
